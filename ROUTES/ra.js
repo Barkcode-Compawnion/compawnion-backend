1eaa0aa471807974e686d8ed01cdfaff00a139c4
barkcode <barkcodecompawnion@gmail.com>
2024-11-05 21:38:19 +0800

    fetch for images in ra.js

--- ROUTES/ra.js
+++ ROUTES/ra.js
@@ -92,7 +92,7 @@ module.exports = function (db, storage) {
             `RescuedAnimals/${formattedPetId}.${type.split("/")[1]}`
           );
           await file.save(buffer, { contentType: type });
-          Picture = `http://localhost:3000/media/RescuedAnimals/${formattedPetId}.${
+          Picture = `gs://compawnion-fbb5a.appspot.com/RescuedAnimals/RescuedAnimals/${formattedPetId}.${
             type.split("/")[1]
           }`;
 
